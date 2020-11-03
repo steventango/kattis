4bfaa8fda0af5a39bae8fc1c714b079c6023d89f
Steven Tang <steventango@users.noreply.github.com>
2020-11-03 14:38:10 -0700

    Add pot.cpp

---
+++ pot.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    using namespace std;
+    int N, X = 0;
+    cin >> N;
+    while (N--)
+    {
+        int P;
+        cin >> P;
+        X += pow(P / 10, P % 10);
+    }
+    cout << X;
+}
