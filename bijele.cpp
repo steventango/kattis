56b432b90c3ce572859f5be79548d11fe1eb05b3
Steven Tang <steventango@users.noreply.github.com>
2020-11-21 16:54:44 -0700

    Add bijele.cpp

---
+++ bijele.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+  int k, q, r, b, n, p;
+  std::cin >> k >> q >> r >> b >> n >> p;
+  std::cout << 1 - k << " " << 1 - q << " " << 2 - r << " " << 2 - b << " "
+            << 2 - n << " " << 8 - p << " " << std::endl;
+}
