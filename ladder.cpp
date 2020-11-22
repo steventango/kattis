489e10b88f86681859dda112fe2bf2c521447055
Steven Tang <steventango@users.noreply.github.com>
2020-11-21 21:06:58 -0700

    Add ladder.cpp

---
+++ ladder.cpp
@@ -0,0 +1,10 @@
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include <iostream>
+
+int main() {
+  double h, v, l;
+  std::cin >> h >> v;
+  l = h / sin(M_PI * v / 180);
+  std::cout << std::ceil(l) << std::endl;
+}
