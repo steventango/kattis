aff505f2b98acf23a4dbf3b6b2bcbcf003d37131
Steven Tang <steventango@users.noreply.github.com>
2020-12-27 22:27:11 -0700

    Add server.cpp

---
+++ server.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+  int n, T, i, sum = 0;
+
+  std::cin >> n >> T;
+
+  for (i = 0; i < n; i++) {
+    int length;
+    std::cin >> length;
+    sum += length;
+    if (sum > T) break;
+  }
+  
+  std::cout << i << std::endl;
+}
