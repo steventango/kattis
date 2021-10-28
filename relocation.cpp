76b31412d7dd740fb9eac4279998d8804e56b2df
Steven Tang <18170455+steventango@users.noreply.github.com>
2021-10-27 20:52:44 -0600

    Use array instead of map

--- relocation.cpp
+++ relocation.cpp
@@ -1,18 +1,21 @@
 #include <cmath>
 #include <iostream>
-#include <unordered_map>
 
 int main() {
   std::ios_base::sync_with_stdio(false);
   std::cin.tie(NULL);
-  std::unordered_map<int, int> locations;
+
   int N, Q;
   std::cin >> N >> Q;
+
+  int locations[N + 1];
+
   for (int i = 1; i <= N; i++) {
     int X;
     std::cin >> X;
     locations[i] = X;
   }
+
   for (int i = 0; i < Q; i++) {
     int T;
     std::cin >> T;
