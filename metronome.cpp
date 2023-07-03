b1dbec78cb2ebf1270a285e46e1f7d9ad29b815e
Steven Tang <18170455+steventango@users.noreply.github.com>
2023-07-02 18:39:03 -0600

    Add metronome.cpp

---
+++ metronome.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(NULL);
+  std::cout.tie(NULL);
+
+  int n;
+  std::cin >> n;
+  std::cout << n / 4. << std::endl;
+}
+
